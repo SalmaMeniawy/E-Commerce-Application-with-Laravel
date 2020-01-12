017c2e204923fd54589d9d49b2983c18d8a046dc
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-12 21:15:57 +0200

    add eloquent relationship in function admin betwen admin and user in User model

--- app/User.php
+++ app/User.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use App\Admin;
 class User extends Authenticatable
 {
     use Notifiable;
@@ -18,7 +18,12 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password','role'
     ];
-
+    /**
+    * eloquent relationship with admin model    
+    */
+    public function admin(){
+        return $this->belongsTo(Admin::class);
+    }
     /**
      * The attributes that should be hidden for arrays.
      *
