0f81dcc0e5102b36dcdd0af6ef75efaa66cc3a46
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-13 16:52:24 +0200

    create store method in store controller to create new store resources in DB

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Store;
 class StoreController extends Controller
 {
     /**
@@ -34,7 +34,11 @@ class StoreController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Store::create([
+           'store_name'  => $request->input('store_name'),
+            'sammary' => $request->input('sammary'),
+        ]);
+        
     }
 
     /**
