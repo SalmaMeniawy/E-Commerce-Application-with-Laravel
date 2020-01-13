2fb8f813d4e36f3a04c4fa3eb97c24052eec159e
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-13 16:24:14 +0200

    start in creating store form

--- resources/views/store/create_store.blade.php
+++ resources/views/store/create_store.blade.php
@@ -1,1 +1,26 @@
-<h1>Hello</h1>
+
+@extends('layouts.app')
+@section('content')
+   
+        <div class="container">
+         <div class="row justify-content-center">
+            <div class="col-md-8">
+             <div class="card">
+                  <div class="card-header">Store Creation Form</div>
+                  <div class="card-body">
+                    <form method="post" action=" ">
+                    @csrf
+                         <div class="form-group row">
+                             <label for="store_name" class="col-md-4 col-form-label text-md-right">Store name                 </label>
+                             <div class="col-md-6">
+                                 <input name="store_name" type="text">
+                             </div>
+                        </div>
+                    </form>
+                 </div>
+              </div>
+           </div>
+         </div>
+        </div>
+   
+@endsection
