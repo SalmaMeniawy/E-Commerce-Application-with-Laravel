57ad5dc3dbad243a6667525ef6f7b609deb807aa
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-13 16:39:00 +0200

    add buttons to store form for creation'

--- resources/views/store/create_store.blade.php
+++ resources/views/store/create_store.blade.php
@@ -15,6 +15,17 @@
                              <div class="col-md-6">
                                  <input name="store_name" type="text">
                              </div>
+                             <label for="sammary" class="col-md-4 col-form-label text-md-right">Sammary                 </label>
+                             <div class="col-md-6">
+                                 <textarea name="sammary">
+                                 </textarea>
+                             </div>
+                            
+                          <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                   Submit
+                                </button>
+                             </div>
                         </div>
                     </form>
                  </div>
