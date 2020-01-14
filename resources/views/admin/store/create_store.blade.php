e5a90bfa657d6cca4b23e0d9b0781fa86bdfeb83
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-14 09:01:35 +0200

    create new folder in views to contain all views related to admin

---
+++ resources/views/admin/store/create_store.blade.php
@@ -0,0 +1,44 @@
+
+@extends('layouts.app')
+@section('content')
+   
+        <div class="container">
+         <div class="row justify-content-center">
+            <div class="col-md-8">
+             <div class="card">
+                  <div class="card-header">Store Creation Form</div>
+                  <div class="card-body">
+                      @if($errors->any())
+                      <ul id="errors">
+                          @foreach($errors->all() as $error)
+                            <li> {{$error}}</li>
+                          @endforeach
+                      </ul>
+                      @endif
+                    <form method="post" action="/stores">
+                    @csrf
+                         <div class="form-group row">
+                             <label for="store_name" class="col-md-4 col-form-label text-md-right">Store name                 </label>
+                             <div class="col-md-6">
+                                 <input name="store_name" type="text">
+                             </div>
+                             <label for="sammary" class="col-md-4 col-form-label text-md-right">Sammary                 </label>
+                             <div class="col-md-6">
+                                 <textarea name="sammary">
+                                 </textarea>
+                             </div>
+                            
+                          <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                   Submit
+                                </button>
+                             </div>
+                        </div>
+                    </form>
+                 </div>
+              </div>
+           </div>
+         </div>
+        </div>
+   
+@endsection
