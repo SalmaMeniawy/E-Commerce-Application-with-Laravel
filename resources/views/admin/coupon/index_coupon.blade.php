9145f229975b295e38f73769d805a6c931980fba
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-17 23:41:00 +0200

    add button to delete coupon and remove column that display the available state in the index

--- resources/views/admin/coupon/index_coupon.blade.php
+++ resources/views/admin/coupon/index_coupon.blade.php
@@ -10,24 +10,31 @@
              	 			@if(isset($coupons))
              	 				<ul  class="list-inline">
              	 					<dl class="row">
-             	 						<dt class="col-md-3">
+             	 						<dt class="col-md-4">
              	 							Coupon name
              	 						</dt>
-             	 						<dt class="col-md-3">
-             	 							Available state
-             	 						</dt>
+             	 						
              	 					</dl>
              	 					@foreach($coupons as $coupon)
              	 					<dl class="row">
              	 						<dt class="col-md-3">
              	 							{{$coupon->coupon_name}}
              	 						</dt>
-             	 						<dt class="col-md-3">
-             	 							{{$coupon->validate_state}}
-             	 						</dt>
+             	 						
              	 						<dd class="col-md-4">
              	 							<a href="{{route('coupon.show',$coupon->id)}}" class="btn btn-info">Show </a>
              	 						</dd>
+             	 						<dt class="col-md-3">
+
+		                                 <form action="{{ route('coupon.destroy', $coupon->id)}}" method="post">
+		                                            @csrf
+		                                            @method('DELETE')
+		                                     <button type="submit" class="btn btn-danger">delete coupon</button>
+		                        
+		                                 </form> 
+									
+						                </dt>
+             	 						
              	 					</dl>
 
              	 					@endforeach
