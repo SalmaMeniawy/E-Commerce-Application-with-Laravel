17f952ac60a301d5ab27fc608c2c043ffb52dc95
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-17 17:00:19 +0200

    add the appilty to make the validation errors in the form to help user to modify his input

--- resources/views/admin/category/create_category.blade.php
+++ resources/views/admin/category/create_category.blade.php
@@ -4,11 +4,23 @@
         <div class="container">
         	<div class="row justify-content-center">
             <div class="col-md-8">
+
              <div class="card">
+             	
                   <div class="card-header">Category Creation Form</div>
                   <div class="card-body">
                   	<form method="POST" action="/categories">
                     @csrf
+                    @if($errors->any())
+             		 <ul id="errors">
+             		 	@foreach($errors->all() as $error)
+             		 		 <div class="alert alert-danger" role="alert">
+                                 {{$error}}
+                             </div>
+             		 	@endforeach
+             		 </ul>
+
+             		@endif
                     	<div class="form-group row">
                     		<label for="category_name" class="col-md-4 col-form-label text-md-right">Category name                 </label>
                     		<div class="col-md-6">
