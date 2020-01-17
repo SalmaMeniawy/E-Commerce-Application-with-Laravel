7745211037583a603b0270da6d6946193cdc9066
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-17 14:28:18 +0200

    create destroy method in brandController

--- app/Http/Controllers/Brand/BrandController.php
+++ app/Http/Controllers/Brand/BrandController.php
@@ -85,8 +85,9 @@ class BrandController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($brand_id)
     {
-        //
+        $brand = Brand::find($brand_id);
+        $brand->delete();
     }
 }
