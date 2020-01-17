8dee63a32cc0f0285123c338f6ec0457161bee24
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-17 19:07:41 +0200

    create migration for coupons table with it is fields

---
+++ database/migrations/2020_01_17_165513_create_coupons_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCouponsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('coupon_name');
+            $table->integer('number_of_usage')->reuire();
+            $table->time('lifetime')->require();
+            $table->float('coupon_persentage')->nullable();
+            $table->float('coupon_price')->nullable();
+            $table->binary('validate_state');
+            $table->binary('apply_coupon');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupons');
+    }
+}
