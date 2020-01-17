7e453701e91ee9b5b53d90f1490bc95d125a2778
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-17 17:33:34 +0200

    create view for show method in category controller

---
+++ resources/views/admin/category/show_category.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+         <div class="row justify-content-center">
+            <div class="col-md-8">
+             <div class="card">
+             	@if(isset($category))
+             		<div class="card">
+             			 <div class="card-header" ><h2>{{$category->category_name}} 	 details</h2>
+             			</div>
+             			<div class="card-block">
+                    <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Category name :{{$category->category_name}}</li>
+                     
+                    
+                    <li class="list-group-item">
+                        Created at : {{$category->created_at}}    
+                    </li>
+                </div>
+             		</div>
+             	@endif
+             </div>
+         </div>
+     </div>
+ </div>
+  @endsection
