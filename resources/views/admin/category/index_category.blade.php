e62a5ad0cb8a108912ca41de641715a3ad9f233e
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-17 17:47:29 +0200

    add route for creating form in category controller

--- resources/views/admin/category/index_category.blade.php
+++ resources/views/admin/category/index_category.blade.php
@@ -4,7 +4,7 @@
         <div class="container">
         	<div class="row justify-content-center">
             <div class="col-md-8">
-
+            	<a href="{{action('Category\CategoryController@create')}}"class="btn btn-primary">Craete new category</a>
             	<div class="card">
              	 <div class="card-header"><h2>Available Categories</h2>
              	 		<div class="card-body">
