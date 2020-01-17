16a25af44733b3c0f993ff03aca2956ba2a12973
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-17 14:25:58 +0200

    create list to show available brands and create button to show brand

---
+++ resources/views/admin/brand/index_brand.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+@section('content')
+	<div class="container">
+         <div class="row justify-content-center">
+            <div class="col-md-8">
+             <div class="card">
+             	 <div class="card-header"><h2>Available Brands</h2></div>
+                  <div class="card-body">
+                  	@if(isset($brands))
+                  	
+                      <ul  class="list-inline">
+                      	@foreach($brands as $brand)
+                      	<dl class="row">
+						<dt class="col-md-3">{{$brand->brand_name}}</dt>
+						<dd class="col-md-4">
+							<a href="action('Brand\BrandController@show',[$brand->id])" class="btn btn-info">Show </a>
+						</dd>
+						<dt class="col-md-3">Address</dt>
+						<dd class="col-md-4">NYC</dd>
+						</dl>
+
+                     	
+                      	@endforeach
+                      </ul>
+                  
+             		@endif    
+                  </div>
+             </div>
+         </div>
+     </div>
+ </div>
+ 
+ @endsection
