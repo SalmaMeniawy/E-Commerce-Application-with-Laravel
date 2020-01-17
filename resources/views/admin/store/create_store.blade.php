02b7780a049ebde1736dfc1ab5e13b7f83bf8d27
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-17 23:55:22 +0200

    add value attribute in the form to catch the old one

--- resources/views/admin/store/create_store.blade.php
+++ resources/views/admin/store/create_store.blade.php
@@ -23,11 +23,11 @@
                          <div class="form-group row">
                              <label for="store_name" class="col-md-4 col-form-label text-md-right">Store name                 </label>
                              <div class="col-md-6">
-                                 <input name="store_name" type="text">
+                                 <input name="store_name" type="text" value="{{old('store_name')}}">
                              </div>
                              <label for="sammary" class="col-md-4 col-form-label text-md-right">Sammary                 </label>
                              <div class="col-md-6">
-                                 <textarea name="sammary">
+                                 <textarea name="sammary" >
                                  </textarea>
                              </div>
                             
