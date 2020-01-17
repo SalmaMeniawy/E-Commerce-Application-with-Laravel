0747f5352b7344b6b22757d8ca85822cfa347f54
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-17 23:06:11 +0200

    add header and display the valide state for each coupon

--- resources/views/admin/coupon/index_coupon.blade.php
+++ resources/views/admin/coupon/index_coupon.blade.php
@@ -9,10 +9,21 @@
              	 		<div class="card-body">
              	 			@if(isset($coupons))
              	 				<ul  class="list-inline">
+             	 					<dl class="row">
+             	 						<dt class="col-md-3">
+             	 							Coupon name
+             	 						</dt>
+             	 						<dt class="col-md-3">
+             	 							Available state
+             	 						</dt>
+             	 					</dl>
              	 					@foreach($coupons as $coupon)
              	 					<dl class="row">
              	 						<dt class="col-md-3">
              	 							{{$coupon->coupon_name}}
+             	 						</dt>
+             	 						<dt class="col-md-3">
+             	 							{{$coupon->validate_state}}
              	 						</dt>
              	 						<dd class="col-md-4">
              	 							<a href="{{route('coupon.show',$coupon->id)}}" class="btn btn-info">Show </a>
