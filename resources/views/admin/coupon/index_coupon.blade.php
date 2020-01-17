4659cb007d56900f35eb5fcd9c0bed8543cf8a69
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-17 23:46:38 +0200

    add route button for creation form related to coupon controller

--- resources/views/admin/coupon/index_coupon.blade.php
+++ resources/views/admin/coupon/index_coupon.blade.php
@@ -4,6 +4,9 @@
         <div class="container">
         	<div class="row justify-content-center">
             <div class="col-md-8">
+            	<div>
+            		<a href="{{route('coupon.create')}}" class="btn btn-primary">Create new coupon</a>
+            	</div>
             	<div class="card">
              	 <div class="card-header"><h2>Available Coupons</h2>
              	 		<div class="card-body">
