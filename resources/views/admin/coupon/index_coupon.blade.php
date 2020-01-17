37256d95e39c9846fdacba5185d6485cfafca3ed
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-17 22:34:21 +0200

    add the coupons in list in index_view and add show link related to coupon controller

--- resources/views/admin/coupon/index_coupon.blade.php
+++ resources/views/admin/coupon/index_coupon.blade.php
@@ -2,5 +2,30 @@
 @section('content')
    
         <div class="container">
+        	<div class="row justify-content-center">
+            <div class="col-md-8">
+            	<div class="card">
+             	 <div class="card-header"><h2>Available Coupons</h2>
+             	 		<div class="card-body">
+             	 			@if(isset($coupons))
+             	 				<ul  class="list-inline">
+             	 					@foreach($coupons as $coupon)
+             	 					<dl class="row">
+             	 						<dt class="col-md-3">
+             	 							{{$coupon->coupon_name}}
+             	 						</dt>
+             	 						<dd class="col-md-4">
+             	 							<a href="" class="btn btn-info">Show </a>
+             	 						</dd>
+             	 					</dl>
+
+             	 					@endforeach
+             	 			@endif
+             	 		</div>
+
+             	 	</div>
+             	 </div>
+            </div>
+        </div>
         </div>
 @endsection
