3290a44c8c72e00dbbbafacb34bba28f37d9ca41
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-17 22:18:39 +0200

    display the error messages related to validation in store method in Coupon controller and add changes to keep the old value in the form

--- resources/views/admin/coupon/create_coupon.blade.php
+++ resources/views/admin/coupon/create_coupon.blade.php
@@ -11,36 +11,44 @@
                   <div class="card-body">
                   	<form method="POST" action="/coupons">
                   		@csrf
+                  		@if($errors->any())
+ 							<ul id="errors">
+             		 		@foreach($errors->all() as $error)
+             		 			 <div class="alert alert-danger" role="alert">
+                                 {{$error}}
+                             </div>
+             		 		@endforeach
+                  		@endif
                   		<div class="form-group row">
 							<label for="coupon_name" class="col-md-4 col-form-label text-md-right">Coupon name                 </label>
 							<div class="col-md-6">
-                                 <input name="coupon_name" type="text">
+                                 <input name="coupon_name" type="text" value="{{old('coupon_name')}}">
                              </div>
                   		</div>
                   		<div class="form-group row">
                   			<label for="number_of_usage" class="col-md-4 col-form-label text-md-right">Usage number               </label>
                   			<div class="col-md-6">
-                                 <input name="number_of_usage" type="number">
+                                 <input name="number_of_usage" type="number" value="{{old('number_of_usage')}}">
                              </div>
                   		</div>
                   		
                   		<div class="form-group row">
                   			<label for="lifetime" class="col-md-4 col-form-label text-md-right">Lifetime for coupon              </label>
                   			<div class="col-md-6">
-                                 <input name="lifetime" type="datetime-local"min="2020-01-07T00:00" max="2025-06-14T00:00">
+                                 <input name="lifetime" type="datetime-local"min="2020-01-07T00:00" max="2025-06-14T00:00" value="{{old('lifetime')}}">
                              </div>
                   		</div>
                   		<div class="form-group row">
                   			<label for="coupon_persentage" class="col-md-4 col-form-label text-md-right">Coupon persentage            </label>
                   			<div class="col-md-6">
-                                 <input name="coupon_persentage" type="number"  step="0.01">
+                                 <input name="coupon_persentage" type="number"  step="0.01" value="{{old('coupon_persentage')}}">
                              </div>
                   		</div>
                   		
                   		<div class="form-group row">
                   			<label for="coupon_price" class="col-md-4 col-form-label text-md-right">Coupon price            </label>
                   			<div class="col-md-6">
-                  				<input type="number"step="0.1" name="coupon_price">
+                  				<input type="number"step="0.1" name="coupon_price" value="{{old('coupon_price')}}">
                   			</div>
                   		</div>
             			<div class="col-md-8 offset-md-4">
