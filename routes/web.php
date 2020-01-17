9f960be0d47d4f9e74166f882a6e8f82ded5221d
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-17 22:39:12 +0200

    add route for show method in Coupon controller

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,7 @@ Route::middleware('auth')->group(function(){
             Route::get('coupons/create' , 'Coupon\CouponController@create')->name('coupon.create');
             Route::post('coupons','Coupon\CouponController@store')->name('coupon.store');
             Route::get('coupons','Coupon\CouponController@index')->name('coupon.index');
+            Route::get('coupons/{coupon_id}','Coupon\CouponController@show')->name('coupon.show');
         });
         //routes for Category Controller
         Route::group([],function(){
