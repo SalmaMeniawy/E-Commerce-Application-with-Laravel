2dfc43c77a5be1f4a1947761d0cc0c3c1fc31a7b
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-18 10:47:55 +0200

    create method admin to describe the eloquent relation with admin model

--- app/Store.php
+++ app/Store.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Admin;
 class Store extends Model
 {
     protected $fillable = ['store_name' , 'sammary','admin_id'];
+    public function admin(){
+        return $this->belongsTo(Admin::class);
+    }
 }
