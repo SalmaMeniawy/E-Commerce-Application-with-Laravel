367b208514f57e52682f6eadff1744137cac1569
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-18 10:51:40 +0200

    create method categories to describe eloquent relation with admin model

--- app/Category.php
+++ app/Category.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Admin;
 class Category extends Model
 {
     protected $fillable = ['category_name','admin_id'];
+    public function admin(){
+        return $this->belongsTo(Admin::class);
+    }
 }
