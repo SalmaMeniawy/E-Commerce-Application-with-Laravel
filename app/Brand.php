5c3a6e7cb3d1bce7c2616a98d1e496ee17fcdc49
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-18 10:43:20 +0200

    create eloquent relation in method admin to describe that each brand belong to one abmin

--- app/Brand.php
+++ app/Brand.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Admin;
 class Brand extends Model
 {
     protected $fillable = ['brand_name','admin_id'];
+    public function admin(){
+        return $this->belongsTo(Admin::class);
+    }
 }
