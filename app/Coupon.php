3c34f593b79dd3d654d45320f77aee68e3fabf51
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-18 10:57:58 +0200

    create method admin to describe the eloquent relation with admin model

--- app/Coupon.php
+++ app/Coupon.php
@@ -3,13 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Admin;
 class Coupon extends Model
 {
     protected $fillable = [
     	'coupon_name','number_of_usage','lifetime','coupon_persentage',
     	'coupon_price','admin_id',
     ];
+    public function admin(){
+        return $this->belongsTo(Admin::class);
+    }
 	/**
      * 
      *get_persentage to convert user input from float 
