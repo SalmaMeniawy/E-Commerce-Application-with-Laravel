fd14abd7847bc16e80f8dcfc818407b6a04abc97
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-18 10:42:25 +0200

    add eloquent relation by creating function brands that descripe thr relation with admin and brand model

--- app/Admin.php
+++ app/Admin.php
@@ -4,10 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\User;
+use App\Brand;
 class Admin extends Model
 {
     protected $fillable = ['fname','lname','date_of_birth','user_id'];
     public function user(){
         return $this->hasOne(User::class);
+    }
+    public function brands(){
+    	return $this->hasMany(Brand::class);
     }
 }
