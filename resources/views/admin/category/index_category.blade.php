f40104b8ffacc5960bb32eebcb29d9898bbfacd4
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-19 16:10:25 +0200

    change the position of button 'create new category'

--- resources/views/admin/category/index_category.blade.php
+++ resources/views/admin/category/index_category.blade.php
@@ -2,9 +2,10 @@
 @section('content')
    
         <div class="container">
+            <a href="{{action('Category\CategoryController@create')}}"class="btn btn-primary">Craete new category</a>
         	<div class="row justify-content-center">
             <div class="col-md-8">
-            	<a href="{{action('Category\CategoryController@create')}}"class="btn btn-primary">Craete new category</a>
+            	
             	<div class="card">
              	 <div class="card-header"><h2>Available Categories</h2>
              	 		<div class="card-body">
