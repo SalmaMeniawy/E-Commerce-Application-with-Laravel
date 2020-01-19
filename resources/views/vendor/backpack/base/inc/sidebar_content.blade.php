43fc4132f422c829b437a855a82e2cd0958b437c
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-19 15:11:42 +0200

    add route for categories to admin dashboard related to categoeies

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -9,4 +9,7 @@ Brands</a></li>
 <li class="nav-item"> <a class="nav-link" 
 href="{{ url('coupons') }}">
 Coupons</a></li>
+<li class="nav-item"> <a class="nav-link" 
+href="{{ url('categories') }}">
+Categories</a></li>
 <li class=nav-item><a class=nav-link href="{{ backpack_url('elfinder') }}"><i class="nav-icon fa fa-files-o"></i> <span>{{ trans('backpack::crud.file_manager') }}</span></a></li>
