d46e05c3ade22a0cacd5af9166fd6de7eee1b197
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-19 14:11:17 +0200

    create redirectTo method to redirect user to itis admin panel

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,13 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
-
+    protected function redirectTo(){
+        if(auth()->user()->role == 'admin'){
+            return '/admin/dashboard';
+        }else{
+            return '/home';
+        }
+    }
     /**
      * Create a new controller instance.
      *
