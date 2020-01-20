20d3605e6fcec52697acd059d822ed823df20308
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-20 18:56:26 +0200

    add eloquent relation to product model with category model

--- app/Product.php
+++ app/Product.php
@@ -4,10 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\brand;
+use App\Category;
 class Product extends Model
 {
     protected $fillable = ['title','description','price','brand_id'];
     public function brand(){
-        return $this->belongTo(Brand::class);
+        return $this->belongsTo(Brand::class);
+    }
+    public function category(){
+        return $this->belongsTo(Category::class);
     }
 }
