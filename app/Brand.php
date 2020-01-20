1d06da8fef160bf7aea9fa2faf0a523bf3db37cd
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-20 17:35:55 +0200

    add eloquent relation to brand model with products model

--- app/Brand.php
+++ app/Brand.php
@@ -4,10 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Admin;
+use App\Product;
 class Brand extends Model
 {
     protected $fillable = ['brand_name','admin_id'];
     public function admin(){
         return $this->belongsTo(Admin::class);
+    }
+    public function products(){
+    	return $this->hasMany(Product::class);
     }
 }
