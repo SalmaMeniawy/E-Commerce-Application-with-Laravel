e602f31d712f46124de02203d5781b46cee1bfc2
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-20 18:16:58 +0200

    add brand_id to fillable in product model

--- app/Product.php
+++ app/Product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use App\brand;
 class Product extends Model
 {
-    protected $fillable = ['title','description','price'];
+    protected $fillable = ['title','description','price','brand_id'];
     public function brand(){
         return $this->belongTo(Brand::class);
     }
