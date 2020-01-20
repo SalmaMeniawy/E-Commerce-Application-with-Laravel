f92b3eae88470b8c54f8f97fab92c860191e628b
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-20 19:40:48 +0200

    add category_id to fillable attribute in product model

--- app/Product.php
+++ app/Product.php
@@ -7,7 +7,8 @@ use App\brand;
 use App\Category;
 class Product extends Model
 {
-    protected $fillable = ['title','description','price','brand_id'];
+    protected $fillable = ['title','description','price',
+    'brand_id','category_id'];
     public function brand(){
         return $this->belongsTo(Brand::class);
     }
