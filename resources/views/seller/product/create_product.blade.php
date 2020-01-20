b767d55ce5c069b9f7189ee0d49c4dfd4f699a12
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-20 16:01:32 +0200

    add validations error to the product creation form

--- resources/views/seller/product/create_product.blade.php
+++ resources/views/seller/product/create_product.blade.php
@@ -9,6 +9,15 @@
              	 <div class="card-body">
              	 	<form method="POST" action="/products">
              	 		@csrf
+             	 		@if($errors->any())
+             	 			<ul id="errors">
+             		 		@foreach($errors->all() as $error)
+             		 			 <div class="alert alert-danger" role="alert">
+                                 {{$error}}
+                             </div>
+             		 		@endforeach
+             		 		</ul>
+             	 		@endif
              	 		<div class="form-group row">
                   			<label for="title" class="col-md-4 col-form-label text-md-right">title          </label>
                   			<div class="col-md-6">
