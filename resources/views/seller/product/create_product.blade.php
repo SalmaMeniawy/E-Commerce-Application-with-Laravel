f4ba877c16deb9d833c9a9810ac0d23ef2f52bea
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-20 18:19:10 +0200

    display the available brands in create product form

--- resources/views/seller/product/create_product.blade.php
+++ resources/views/seller/product/create_product.blade.php
@@ -30,7 +30,18 @@
                                  <textarea name="description"></textarea>  
                              </div>
                   		</div>
-             	 		
+             	 		<div class="form-group row">
+						  <label for="brand" class="col-md-4 col-form-label text-md-right">brand  </label>
+						  <div class="col-md-6">
+						  <select class="col-md-6"  name="brand_id">
+                                @foreach($brands as $brand)
+								<option value="{{$brand->id}}">{{$brand->brand_name}}</option>
+
+								@endforeach
+								</select>
+                             </div>
+							
+             	 		</div>
              	 		<div class="form-group row">
                   			<label for="coupon_persentage" class="col-md-4 col-form-label text-md-right">price           </label>
                   			<div class="col-md-6">
