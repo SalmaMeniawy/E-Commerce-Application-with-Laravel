0f7c34d8438870c4edb558af3ec700c753e574ad
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-20 11:40:11 +0200

    create method logout to help the admin to logout and redirect him to the home page

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class AdminController extends Controller
 {
     /**
@@ -16,7 +16,10 @@ class AdminController extends Controller
     {
         //
     }
-
+    public function logOut(){
+        Auth::logout();
+        return redirect()->route('home');
+    }
     /**
      * Show the form for creating a new resource.
      *
