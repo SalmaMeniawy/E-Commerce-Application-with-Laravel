7301ee9054bda0572b890bdfb40f065bf36aaad9
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-21 17:01:13 +0200

    add new field in_stock_quantity in products table in new migration

---
+++ database/migrations/2020_01_21_145843_add_in_stock_quantity_to_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddInStockQuantityToProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->integer('in_stock_quantity')->require();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+}
