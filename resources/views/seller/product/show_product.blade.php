7fc86020b99a0f5160c5849666f27f5842075ce9
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-21 16:53:47 +0200

    create show view with product destails related to product controller

--- resources/views/seller/product/show_product.blade.php
+++ resources/views/seller/product/show_product.blade.php
@@ -1,1 +1,36 @@
-<h1></h1>
+@extends('layouts.app')
+
+
+@section('content')
+<div class="container">
+         <div class="row justify-content-center">
+            <div class="col-md-8">
+             <div class="card">
+             @if(isset($product)&& isset($product_category_name))
+             <div class="card">
+             <div class="card-header" ><h2>{{$product->title}} product	 details</h2>
+                <div class="card-block">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        Product title : {{$product->title}}
+                    </li>
+                    <li class="list-group-item">
+                        Product category : {{$product_category_name}}
+                    </li>
+                    <li class="list-group-item">
+                        Product description : {{$product->description}}
+                    </li>
+                    <li class="list-group-item">
+                        Product price : {{$product->price}}
+                    </li>
+                </ul>
+
+                </div>
+             </div>
+             </div>
+             @endif
+             </div>
+            </div>
+         </div>
+</div>
+@endsection
