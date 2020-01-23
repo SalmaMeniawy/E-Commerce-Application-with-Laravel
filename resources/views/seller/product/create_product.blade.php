76fa0ee759694f0b276d8c3076c21c2b76a65560
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-23 14:07:06 +0200

    add sidebar for create new product form

--- resources/views/seller/product/create_product.blade.php
+++ resources/views/seller/product/create_product.blade.php
@@ -1,5 +1,5 @@
-@extends('layouts.app')
-
+@extends(backpack_view('blank'))
+@section('content')
 @section('content')
         <div class="container">
          <div class="row justify-content-center">
