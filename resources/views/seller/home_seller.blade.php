cef604f59717f97e23d92bd8bc8eceb31439cb71
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-23 14:01:14 +0200

    add slider to display images in seller home page

--- resources/views/seller/home_seller.blade.php
+++ resources/views/seller/home_seller.blade.php
@@ -1,23 +1,27 @@
 @extends(backpack_view('blank'))
 @section('content')
-<h1>Hello in Seller header_remove</h1>
-<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="..." alt="First slide">
+      <img class="d-block w-100" src="{{url('/seller_homepage/image1.jpg')}}" alt="First slide" width="600" height="600">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Second slide">
+      <img class="d-block w-100" src="{{url('/seller_homepage/image2.jpg')}}" alt="Second slide" width="600" height="600">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Third slide">
+      <img class="d-block w-100" src="{{url('/seller_homepage/image3.jpg')}}" alt="Third slide" width="600" height="600">
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
