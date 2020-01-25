084d592dbf3fd06796c01f18943044a17c7c4161
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-25 20:00:51 +0200

    create buyer homepage display product name and image

--- resources/views/buyer/index_buyer.blade.php
+++ resources/views/buyer/index_buyer.blade.php
@@ -1,5 +1,32 @@
 @extends(backpack_view('blank'))
 @section('content')
+<div class="container " >
+    <div class="card-group">
+        @foreach($products as $product) 
+        <table class="table-responsive-lg">
+        
+        <tbody>
+        <tr class="d-flex">
+        <th style="width: 100%">
+        <div class="card" >
+            <div class="card-header" ><h4>{{$product->title}}</h4> </div>
+            <img src='{{url("/productImages",$product->image)}}' width="200" height="200" class="align-middle"> 
 
+            <div class="card-block">
+            
+                <a href="" class="btn btn-info"> Details</a>
+                <a href="" class="btn btn-info"> Details</a>
+            
+            </div>
+
+        </div>
+        </th>
+        </tr>
+        </tbody>
+        </table>
+
+        @endforeach
+    </div>
+</div>
 
 @endsection
