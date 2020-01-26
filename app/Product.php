c972d2dbeffb091ee681073fd77a24fe125c7b48
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-26 19:11:40 +0200

    create method shoppingCarts as eloquent function with shopping cart model in product model

--- app/Product.php
+++ app/Product.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 use App\Brand;
 use App\Category;
 use App\Seller;
+use App\ShoppingCart;
 class Product extends Model
 {
     protected $fillable = ['title','description','price',
@@ -18,5 +19,8 @@ class Product extends Model
     }
     public function seller(){
         return $this->belongsTo(Seller::class);
+    }
+    public function shoppingCarts(){
+        return $this->hasMany(ShoppingCart::class);
     }
 }
