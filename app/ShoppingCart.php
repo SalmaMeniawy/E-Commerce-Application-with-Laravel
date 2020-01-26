bdaac3c5b4279936c4aaae28ac3dad551886ac13
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-26 15:46:38 +0200

    create eloquent relational method with brand model

--- app/ShoppingCart.php
+++ app/ShoppingCart.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Brand;
 class ShoppingCart extends Model
 {
     protected $fillable = ['product_id','buyer_id','quantity'];
+    public function buyer(){
+        return $this->belongsTo(Brand::class);
+    }
 }
