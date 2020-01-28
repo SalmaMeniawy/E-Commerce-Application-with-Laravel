629f702c50a1dc28c3569ec63ae7c5f327b63e19
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-28 14:46:34 +0200

    create get_product_id_from_shopping_cart static method to get product_quantity in array

--- app/ShoppingCart.php
+++ app/ShoppingCart.php
@@ -11,6 +11,12 @@ class ShoppingCart extends Model
     public function buyer(){
         return $this->belongsTo(Brand::class);
     }
+    public static function get_product_id_from_shopping_cart(){
+        $product_quantity= ShoppingCart::get()->where('buyer_id',auth()->id())[0]->product_quantity;
+        $product_quanyity_in_array = json_decode($product_quantity,true); //add true to convert string json to array ^^
+        
+        return $product_quanyity_in_array;
+    }
     public static function check_if_product_added_before_and_return_it(int $product_id){
            $product = ShoppingCart::get()->where('buyer_id',auth()->id())->where('product_id',$product_id)->first();
            if(isset($product)){
