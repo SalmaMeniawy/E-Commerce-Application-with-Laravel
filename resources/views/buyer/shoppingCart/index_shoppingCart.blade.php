c8957ff63612c96806d00ab1bfe0581af4f30c9a
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-30 10:48:43 +0200

    display the product details

--- resources/views/buyer/shoppingCart/index_shoppingCart.blade.php
+++ resources/views/buyer/shoppingCart/index_shoppingCart.blade.php
@@ -13,20 +13,25 @@
                         <th> </th>
                     </tr>
                 </thead>
-              
+                @foreach($products as $product  )  
+               
                 <tbody>
                     <tr>
                         <td class="col-sm-8 col-md-6">
                         <div class="media">
                             <a class="thumbnail pull-left" href="#"> <img class="media-object" src="http://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/72/product-icon.png" style="width: 72px; height: 72px;"> </a>
+                          
                             <div class="media-body">
-                                <h4 class="media-heading"><a href="#">Product name</a></h4>
+                                <h4 class="media-heading"><a href="#">{{$product->title}}</a></h4>
                                 <h5 class="media-heading"> by <a href="#">Brand name</a></h5>
                                 <span>Status: </span><span class="text-success"><strong>In Stock</strong></span>
                             </div>
+                           
                         </div></td>
                         <td class="col-sm-1 col-md-1" style="text-align: center">
-                        <input type="email" class="form-control" id="exampleInputEmail1" value="3">
+                        
+                        <input type="email" class="form-control" id="exampleInputEmail1" value="{{$product_quantity[$product->id]}}">
+
                         </td>
                         <td class="col-sm-1 col-md-1 text-center"><strong>$4.87</strong></td>
                         <td class="col-sm-1 col-md-1 text-center"><strong>$14.61</strong></td>
@@ -35,7 +40,7 @@
                             <span class="glyphicon glyphicon-remove"></span> Remove
                         </button></td>
                     </tr>
-                
+                    @endforeach
                     
                     <tr>
                         <td>   </td>
