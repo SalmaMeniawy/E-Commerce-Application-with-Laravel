26892d326f5bb3d049c0c63734a340a1e8d942c0
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-01-30 20:10:25 +0200

    display the return from check_product_quantity_stock method

--- resources/views/buyer/shoppingCart/index_shoppingCart.blade.php
+++ resources/views/buyer/shoppingCart/index_shoppingCart.blade.php
@@ -24,7 +24,12 @@
                             <div class="media-body">
                                 <h4 class="media-heading"><a href="#">{{$product->title}}</a></h4>
                                 <h5 class="media-heading"> by <a href="#">Brand name</a></h5>
-                                <span>Status: </span><span class="text-success"><strong>In Stock</strong></span>
+                                @if($in_stock_state[$product->id] == 1)
+                                <span>Status: </span><span class="text-success"><strong>In Stock </strong></span>
+                                @else
+                                <span>Status: </span><span class="text-danger"><strong>Out of Stock </strong></span>
+
+                                @endif
                             </div>
                            
                         </div></td>
