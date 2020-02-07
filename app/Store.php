cc2dc1b5c0c82863443bcdd30408581a730e2e05
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-02-07 15:48:53 +0200

    add eloquent relation with seller model

--- app/Store.php
+++ app/Store.php
@@ -4,10 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Admin;
+use App\Seller;
 class Store extends Model
 {
     protected $fillable = ['store_name' , 'sammary','admin_id'];
     public function admin(){
         return $this->belongsTo(Admin::class);
+    }
+    public function seller(){
+        return $this->belongsTo(Seller::class);
     }
 }
