6a3048d4597849b7decb5a64d0206c1b41af2765
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-02-16 20:39:36 +0200

    add the content of BuyerCategories table

--- database/seeds/BuyerCategoriesTableSeeder.php
+++ database/seeds/BuyerCategoriesTableSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 
 class BuyerCategoriesTableSeeder extends Seeder
 {
+    protected $available_buyer_category_names = ['Gold','Silver','Bronz','normal'];
     /**
      * Run the database seeds.
      *
@@ -11,6 +12,11 @@ class BuyerCategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+       foreach($this->available_buyer_category_names as $item){
+        DB::table('buyer_categories')->insert([
+            'buyer_category_name'=>$item,
+        ]);
+       }
+       
     }
 }
