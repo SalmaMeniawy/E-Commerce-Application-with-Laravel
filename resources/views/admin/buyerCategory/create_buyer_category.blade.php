caf7b5bec7f343c1d816cdfbdc9288e7c3f5df41
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-02-16 16:11:16 +0200

    add route of store method in the action of the create buyer category form

--- resources/views/admin/buyerCategory/create_buyer_category.blade.php
+++ resources/views/admin/buyerCategory/create_buyer_category.blade.php
@@ -8,7 +8,7 @@
                     <h2>Create Buyer Category</h2>
                 </div>
                 <div class="card-body">
-                    <form method="POST" action="">
+                    <form method="POST" action="/buyerCategory">
                     @csrf
                     <div class="form-group row">
                     <label for="brand_name" class="col-md-4 col-form-label text-md-right">Buyer Category Name                </label>
