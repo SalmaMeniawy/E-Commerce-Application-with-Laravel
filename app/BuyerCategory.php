c8b18469195a1d6c43cbf278e9c6865811132e78
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-02-16 19:26:47 +0200

    add eloquent relational function with buyers in BuyerCategory model

--- app/BuyerCategory.php
+++ app/BuyerCategory.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Buyer;
 class BuyerCategory extends Model
 {
     protected $fillable = ['buyer_category_name'];
+    public function buyers(){
+        return $this->hasMany(Buyer::class);
+    }
 }
