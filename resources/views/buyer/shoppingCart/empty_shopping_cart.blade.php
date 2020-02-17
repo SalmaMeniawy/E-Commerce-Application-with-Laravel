39a94e8eb0877c29fb7f7f082b046ed1b2534be5
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-02-17 14:29:57 +0200

    add view to display that the shopping cart is empty

---
+++ resources/views/buyer/shoppingCart/empty_shopping_cart.blade.php
@@ -0,0 +1,16 @@
+@extends(backpack_view('blank'))
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-10 col-md-offset-1">
+            <table class="table table-hover">
+                <thead  class="p-3 mb-2 bg-info  text-dark">
+                    <div class="alert alert-danger" role="alert">
+                        <strong>Oh sorry!</strong> {{$empty_shopping_cart}}
+                    </div>
+                </thead>
+            </table>
+        </div>
+    </div>
+</div>        
+@endsection
