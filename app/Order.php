adcbed90b725f52dc842dd03527113589f13b64e
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-03-11 19:03:58 +0200

    add fillable fields for order table in order model

--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = ["address_for_shipping","telephone_for_shipping",
+        "total_order_items_quantity","total_order_price","order_items"];
 }
