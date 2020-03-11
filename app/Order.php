0f594bde6f3af1e54cd5445010c208a7a482d8a5
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-03-11 19:06:51 +0200

    add eloquent function for order with products in order model

--- app/Order.php
+++ app/Order.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Product;
 class Order extends Model
 {
     protected $fillable = ["address_for_shipping","telephone_for_shipping",
         "total_order_items_quantity","total_order_price","order_items"];
+    public function products(){
+        return $this->belongsToMany(Product::class);
+    }
 }
