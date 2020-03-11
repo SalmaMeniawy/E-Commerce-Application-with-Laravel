d87a3f00bbf9e142b39551149ee6349e8f5f1619
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-03-11 18:56:31 +0200

    create migration to create order table in the database

---
+++ database/migrations/2020_03_11_162230_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('address_for_shipping');
+            $table->string('telephone_for_shipping');
+            $table->integer('total_order_items_quantity');
+            $table->enum('order_state',["Pending","Confirmed","Delivered"])
+                ->default("Pending");
+            $table->float('total_order_price');
+            $table->string('order_items');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
