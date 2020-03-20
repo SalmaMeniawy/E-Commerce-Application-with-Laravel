a4fb11eb87ea592bd607bca17f5e7f0f9e0f74e4
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-03-20 21:19:07 +0200

    add buyer_id field to fillable array in order model

--- app/Order.php
+++ app/Order.php
@@ -8,7 +8,7 @@ use App\Buyer;
 class Order extends Model
 {
     protected $fillable = ["address_for_shipping","telephone_for_shipping",
-        "total_order_items_quantity","total_order_price","order_items"];
+        "total_order_items_quantity","total_order_price","order_items","buyer_id"];
     public function products(){
         return $this->belongsToMany(Product::class);
     }
