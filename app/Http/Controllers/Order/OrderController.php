3c4b55f5d82ace1102f031622c1c1660c8ebd29b
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-03-22 19:13:06 +0200

    add logic to handle if the user  will add coupon to its order

--- app/Http/Controllers/Order/OrderController.php
+++ app/Http/Controllers/Order/OrderController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Product;
 use App\Buyer;
 use App\Order;
+
 class OrderController extends Controller
 {
     /**
@@ -25,24 +26,23 @@ class OrderController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function create(Request $request)
-    {   
+    {
         $product_id = $request->input('product_id');
         $quantity_of_products = $request->input('quantity_of_products');
         $product_quantity = [];
-        $product_quantity = array_combine($product_id , $quantity_of_products);
+        $product_quantity = array_combine($product_id, $quantity_of_products);
         $products = Product::find($product_id);
-        $total_for_each_prooduct = Order::calculate_total_price_for_each_product_order($products,$product_quantity);
+        $total_for_each_prooduct = Order::calculate_total_price_for_each_product_order($products, $product_quantity);
         $total_for_order_before_coupon = Order::calculate_subtotal_price_for_all_products_in_order($total_for_each_prooduct);
         return view("buyer.order.create_order")
         ->with(compact(['products','product_quantity','total_for_order_before_coupon']));
-       
     }
-    public function create_order(Request $request){
+    public function create_order(Request $request)
+    {
         $products_id = $request->input('products_id') ;
         $quantity_of_products = $request->input('quantity');
         $data = ['product_id'=>$products_id,'quantity_of_products'=>$quantity_of_products];
-        return route('create_new_order',$data);
-
+        return route('create_new_order', $data);
     }
     /**
      * Store a newly created resource in storage.
@@ -52,7 +52,7 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-       $request->validate([
+        $request->validate([
             'tele' => 'required|integer',
             'street_no' =>'required|integer',
             'street_name' =>'required',
@@ -61,6 +61,9 @@ class OrderController extends Controller
        ]);
         $product_id = array_keys($request->input('product_quantity'));
         $quantity = array_values($request->input('product_quantity'));
+        $coupon_id_from_form =$request->input('coupon_id');
+        // $coupon_id_from_form = (isset($request->input('coupon_id')))
+        $order_price_after_coupon_value =$request->input('order_price_after_coupon_value');
         $street_no = $request->input('street_no');
         $street_name = $request->input('street_name');
         $city = $request->input('city');
@@ -68,18 +71,26 @@ class OrderController extends Controller
         $order_items = Order::get_the_count_of_items_in_order($quantity); //get_count of the order items
         dump($request->input());
         $buyer = Buyer::get()->where('user_id', auth()->id())->first();
-        
-        $order = Order::create([
-            'telephone_for_shipping'=>$request->input('tele'),
-            'total_order_price'=>$request->input('total_for_order_before_coupon'),
-            'total_order_items_quantity' =>$order_items,
-            'buyer_id'=>$buyer->id,
-            'order_items' =>\json_encode($quantity) ,
-            'address_for_shipping'=>$full_address,
+      
+            $order = new Order();
+            $order->telephone_for_shipping = $request->input('tele');
+            $order->total_order_price = $request->input('total_for_order_before_coupon');
+            $order->total_order_items_quantity = $order_items;
+            $order->buyer_id = $buyer->id;
+            $order->order_items =json_encode($quantity) ;
+            $order->address_for_shipping = $full_address;
+            if($coupon_id_from_form != 0){
+                $order->coupon_id = $coupon_id_from_form;
+                $order->order_price_after_coupon_value = $order_price_after_coupon_value;
+                $order->save();
+                $order->products()->sync(\json_encode($product_id));
 
-        ]);
-        
-        $order->products()->sync(\json_encode($product_id));
+             }else{
+                 $order->save();
+                 $order->products()->sync(\json_encode($product_id));
+
+             }
+           
     }
 
     /**
