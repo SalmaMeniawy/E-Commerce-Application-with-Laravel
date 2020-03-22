7da64f60ccb49df575f77d7314d9ac56d605a13f
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-03-22 23:11:46 +0200

    add route for index method in Order controller

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,7 @@ Route::middleware('auth')->group(function(){
             Route::post('create_order','Order\OrderController@create_order')->name('create_order');
             Route::get('create_new_order','Order\OrderController@create')->name('create_new_order');
             Route::post('orders','Order\OrderController@store')->name('store_order');
+            Route::get('orders','Order\OrderController@index')->name('index_order');
         });
         Route::get('homepage','Buyer\BuyerController@index')->name('buyer.index');
         Route::get('product/{product_id}','Buyer\BuyerController@get_product_details')->name('buyer.product.show');
