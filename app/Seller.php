e66676078479a11c72fbc7626f8107ea5036cede
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-03-25 15:10:39 +0200

    create method get_all_seller_orders

--- app/Seller.php
+++ app/Seller.php
@@ -18,4 +18,21 @@ class Seller extends Model
     public function store(){
         return $this->hasOne(Store::class);
     }
+    /**
+     * function that return all seller orders 
+     * @return array with all seller orders
+     */
+    public function get_all_seller_orders(){
+        $orders = Order::all();
+        $seller_orders = [];
+        foreach($orders as $order){
+            foreach($order->products as $product){
+               
+                if($product->seller_id == $this->id){
+                    array_push($seller_orders,$order);
+                } 
+            }
+        }
+        return $seller_orders;
+    }
 }
