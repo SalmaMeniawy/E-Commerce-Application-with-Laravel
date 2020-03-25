e4ec68f3f9f94dd16b5c62f971bbb62eb5609495
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-03-25 18:48:14 +0200

    create method get_count_of_pending_products_in_the_orders to get the items that need to confirm

--- app/Seller.php
+++ app/Seller.php
@@ -35,4 +35,20 @@ class Seller extends Model
         }
         return $seller_orders;
     }
+    /**
+     * function to return count of products in orders that need to confirm
+     */
+    public function get_count_of_pending_products_in_the_orders(){
+        $seller = Seller::where('user_id',auth()->id())->first();
+        $orders = $seller->get_all_seller_orders(); 
+        $pending_orders = 0;
+        foreach($orders as $order){
+            foreach($order->products as $product){
+                if($product->pivot->order_state_for_seller == "Pending"){
+                    $pending_orders = $pending_orders + 1;
+                }
+            }
+        }
+        return $pending_orders;
+    }
 }
