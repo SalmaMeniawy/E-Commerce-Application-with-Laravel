7225c1369145be3f396df60c30565a9155202365
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-03-28 19:40:52 +0200

    use the custom layout to display register and login view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,27 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" href="{{ asset('css/app.css') }}">
-   
-    
-</script>
-</head>
-
-<body style="background-image: url('app_backgrounds/image.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
-<div class="container">
-          
-          <div class="row py-5">
-               
-                <div class="col-md-12 text-bold text-center">
-                    <labrl>
-                      <h1>  Welcome in LaraApp</h1>
-                    </labrl>
-                </div>
-
-          </div>
-         
-</div>
-<div class="container">
+@extends('layouts.laraApp.app')
+@section('content')
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -136,6 +114,6 @@
                 </div>
             </div>
         </div>
-    </div>
-</div>
+
+@endsection
 
